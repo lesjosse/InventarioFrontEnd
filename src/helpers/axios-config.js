22cae0b35d891b7ed782e1a82978bc404243c0fb
LesJosse <josajosa97@outlook.com>
2022-10-04 17:34:31 -0500

    agregando url

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://inventario-fullstack-iud.herokuapp.com/'
 })
 
 export{
